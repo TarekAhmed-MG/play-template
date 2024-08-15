c508dac610f8023743d00b278426b4ac7b022fe5
Tarek Ahmed <tarek.ahmed@mercator.group>
2024-08-15 18:39:10 +0100

    RepositoryServiceSpec tests complete CRUD and Index methods in service both Right and Left cases accounted for

--- app/services/RepositoryService.scala
+++ app/services/RepositoryService.scala
@@ -7,18 +7,18 @@ import repositories.{DataRepository, dataRepositoryTrait}
 import javax.inject.Inject
 import scala.concurrent.Future
 
-class RepositoryService @Inject()(val dataRepository: dataRepositoryTrait){
+class RepositoryService @Inject()(val dataRepositoryTrait: dataRepositoryTrait){
 
-  def index(): Future[Either[APIError.BadAPIResponse, Seq[DataModel]]] = dataRepository.index()
+  def index(): Future[Either[APIError.BadAPIResponse, Seq[DataModel]]] = dataRepositoryTrait.index()
 
-  def create(book:DataModel): Future[Either[APIError.BadAPIResponse, InsertOneResult]] = dataRepository.create(book)
+  def create(book:DataModel): Future[Either[APIError.BadAPIResponse, InsertOneResult]] = dataRepositoryTrait.create(book)
 
-  def read(id:String): Future[Either[APIError.BadAPIResponse, Some[DataModel]]] = dataRepository.read(id)
+  def read(id:String): Future[Either[APIError.BadAPIResponse, Some[DataModel]]] = dataRepositoryTrait.read(id)
 
-  def update(id:String,fieldName:String,book:DataModel): Future[Either[APIError.BadAPIResponse, UpdateResult]] = dataRepository.update(id,fieldName,book)
+  def update(id:String,fieldName:String,book:DataModel): Future[Either[APIError.BadAPIResponse, UpdateResult]] = dataRepositoryTrait.update(id,fieldName,book)
 
-  def delete(id:String): Future[Either[APIError.BadAPIResponse, DeleteResult]] = dataRepository.delete(id)
+  def delete(id:String): Future[Either[APIError.BadAPIResponse, DeleteResult]] = dataRepositoryTrait.delete(id)
 
-  def deleteAll(): Future[Unit] = dataRepository.deleteAll()
+  def deleteAll(): Future[Unit] = dataRepositoryTrait.deleteAll()
 
 }
--- test/controllers/RepositoryServiceSpec.scala
+++ test/controllers/RepositoryServiceSpec.scala
@@ -1,12 +1,20 @@
 package controllers
 
 import baseSpec.BaseSpec
+import cats.data.EitherT
+import com.mongodb.client.result.{DeleteResult, InsertOneResult, UpdateResult}
 import models.{APIError, DataModel}
+import org.mongodb.scala.bson.{BsonObjectId, BsonValue, ObjectId}
+import org.mongodb.scala.result.InsertOneResult
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.http.Status
 import play.api.http.Status.CREATED
+import play.api.libs.json.{JsValue, OFormat}
 import play.api.mvc.Results.Status
+import play.api.test.FakeRequest
+import play.api.test.Helpers.status
 import repositories.{DataRepository, dataRepositoryTrait}
 import services.RepositoryService
 import uk.gov.hmrc.mongo.MongoComponent
@@ -32,52 +40,135 @@ class RepositoryServiceSpec extends BaseSpec with MockFactory with ScalaFutures 
 
     "Index() should return books" in {
 
+      (mockDataRepositoryTrait.index: ()=> Future[Either[APIError.BadAPIResponse, Seq[DataModel]]])
+        .expects()
+        .returning(Future(Right(Seq(dataModel))))
+        .once()
+
+      whenReady(testService.index()) { result =>
+        result shouldBe Right(Seq(dataModel))
+      }
+
     }
 
     "Index() should return Api Error" in {
 
+      (mockDataRepositoryTrait.index: ()=> Future[Either[APIError.BadAPIResponse, Seq[DataModel]]])
+        .expects()
+        .returning(Future(Left(APIError.BadAPIResponse(404, "Books cannot be found"))))
+        .once()
+
+      whenReady(testService.index()) { result =>
+        result shouldBe Left(APIError.BadAPIResponse(404, "Books cannot be found"))
+      }
     }
+
 
     "Create() should return result" in {
 
-//
-//      val createBookModel = testService.create(dataModel).map {
-//        case Right(_) => Status(CREATED)
-//        case Left(apiError) => Status(apiError.upstreamStatus)(apiError.upstreamMessage)
-//      }
-//
-//      whenReady(createBookModel) { result =>
-//        result shouldBe Status(CREATED)
-//      }
+      val testObject = BsonObjectId(new ObjectId())
+
+      (mockDataRepositoryTrait.create(_:DataModel))
+        .expects(dataModel)
+        .returning(Future(Right(InsertOneResult.acknowledged(testObject))))
+        .once()
+
+      whenReady(testService.create(dataModel)) { result => // had to remove the .value
+        result shouldBe Right(InsertOneResult.acknowledged(testObject))
+      }
 
     }
 
+
     "Create() should return Api Error" in {
+
+      (mockDataRepositoryTrait.create(_:DataModel))
+        .expects(dataModel)
+        .returning(Future(Left(APIError.BadAPIResponse(500, "Could not Create Book"))))
+        .once()
+
+      whenReady(testService.create(dataModel)) { result => // had to remove the .value
+        result shouldBe Left(APIError.BadAPIResponse(500, "Could not Create Book"))
+      }
 
     }
 
     "Read() should return Book" in {
 
+      (mockDataRepositoryTrait.read(_:String))
+        .expects("abcd")
+        .returning(Future(Right(Some(dataModel))))
+        .once()
+
+      whenReady(testService.read("abcd")) { result => // had to remove the .value
+        result shouldBe Right(Some(dataModel))
+      }
+
     }
 
     "Read() should return Api Error" in {
+
+      (mockDataRepositoryTrait.read(_:String))
+        .expects("abcd")
+        .returning(Future(Left(APIError.BadAPIResponse(404, "Unable to find any books"))))
+        .once()
+
+      whenReady(testService.read("abcd")) { result => // had to remove the .value
+        result shouldBe Left(APIError.BadAPIResponse(404, "Unable to find any books"))
+      }
 
     }
 
     "Update() should return return result" in {
 
+      val testObject = BsonObjectId(new ObjectId())
+
+      (mockDataRepositoryTrait.update(_:String,_:String,_:DataModel))
+        .expects("test1","test2",dataModel)
+        .returning(Future(Right(UpdateResult.acknowledged(1L,1L,testObject))))
+        .once()
+
+      whenReady(testService.update("test1","test2",dataModel)) { result => // had to remove the .value
+        result shouldBe Right(UpdateResult.acknowledged(1L,1L,testObject))
+      }
+
     }
 
     "Update() should return Api Error" in {
+
+      (mockDataRepositoryTrait.update(_:String,_:String,_:DataModel))
+        .expects("test1","test2",dataModel)
+        .returning(Future(Left(APIError.BadAPIResponse(404, "Unable to Update Book"))))
+        .once()
+
+      whenReady(testService.update("test1","test2",dataModel)) { result => // had to remove the .value
+        result shouldBe Left(APIError.BadAPIResponse(404, "Unable to Update Book"))
+      }
 
     }
 
     "Delete() should return result" in {
 
+      (mockDataRepositoryTrait.delete(_:String))
+        .expects("abcd")
+        .returning(Future(Right(DeleteResult.acknowledged(1))))
+        .once()
+
+      whenReady(testService.delete("abcd")) { result => // had to remove the .value
+        result shouldBe Right(DeleteResult.acknowledged(1))
+      }
     }
 
     "Delete() should return Api Error" in {
 
+      (mockDataRepositoryTrait.delete(_:String))
+        .expects("abcd")
+        .returning(Future(Left(APIError.BadAPIResponse(404, "Unable to Delete Book"))))
+        .once()
+
+      whenReady(testService.delete("abcd")) { result => // had to remove the .value
+        result shouldBe Left(APIError.BadAPIResponse(404, "Unable to Delete Book"))
+      }
     }
 
   }
